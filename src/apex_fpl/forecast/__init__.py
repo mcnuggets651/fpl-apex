132f95babfac68fb995e42e9aef182df8823e1ce
mcnuggets651 <chatgptbc2025@gmail.com>
2026-08-24 09:25:45 +0100

    v2 slice 7: expose sealed forecast APIs

--- src/apex_fpl/forecast/__init__.py
+++ src/apex_fpl/forecast/__init__.py
@@ -1,5 +1,24 @@
 """Sealed probabilistic forecast layer for Apex V2."""
 
+from .engine import compile_sealed_forecast
+from .forecast_store import StoredForecast, load_forecast, store_forecast
+from .prediction_store import (
+    StoredPredictionBatch,
+    load_prediction_batch,
+    store_prediction_batch,
+)
 from .targets import OfficialForecastTargetSet, build_official_forecast_targets
+from .validation import validate_prediction_batch_safety
 
-__all__ = ["OfficialForecastTargetSet", "build_official_forecast_targets"]
+__all__ = [
+    "OfficialForecastTargetSet",
+    "StoredForecast",
+    "StoredPredictionBatch",
+    "build_official_forecast_targets",
+    "compile_sealed_forecast",
+    "load_forecast",
+    "load_prediction_batch",
+    "store_forecast",
+    "store_prediction_batch",
+    "validate_prediction_batch_safety",
+]
